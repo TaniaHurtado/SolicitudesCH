9f88f1a442915327f1cbda944b1130e22423ee9f
Jess Casas <jlcasasg@unal.edu.co>
2018-06-04 16:09:48 -0500

    filter admin solicituds by state

--- app/models/solicitud.rb
+++ app/models/solicitud.rb
@@ -3,4 +3,16 @@ class Solicitud < ApplicationRecord
   belongs_to :ubicacion
   has_one :evaluacion
   #has_many :user
+
+  def self.solicitudes_por_estado(estado)
+    if estado == nil
+      all
+    else
+      if estado == "Todas"
+        where("") #Return all columns
+      else
+        where("estado = ?", estado) #Return an especified state
+      end
+    end
+  end
 end
