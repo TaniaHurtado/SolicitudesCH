c65c62f0e897e38cfff7e77b8a43f0057d4aae58
Jess Casas <jlcasasg@unal.edu.co>
2018-06-04 20:33:56 -0500

    update filters by solicitante, lugar, importancia and responsable

--- app/models/solicitud.rb
+++ app/models/solicitud.rb
@@ -4,15 +4,19 @@ class Solicitud < ApplicationRecord
   has_one :evaluacion
   #has_many :user
 
-  def self.solicitudes_por_estado(estado)
-    if estado == nil
-      all
-    else
-      if estado == "Todas"
-        where("") #Return all columns
-      else
-        where("estado = ?", estado) #Return an especified state
-      end
-    end
+  def self.solicitudes_filtro(
+    estado = nil, 
+    importancia = nil, 
+    solicitante = nil,
+    responsable = nil,   
+    lugar = nil
+    )
+    solicitud = all
+    solicitud = solicitud.where(estado: estado) if estado && estado != ""
+    solicitud = solicitud.where(importancia: importancia) if importancia && importancia != ""
+    solicitud = solicitud.where(nombre_responsable: responsable) if responsable && responsable != ""
+    solicitud = solicitud.where(user: solicitante) if solicitante && solicitante != ""
+    solicitud = solicitud.where(lugar: lugar) if lugar && lugar != ""
+    return solicitud
   end
 end
