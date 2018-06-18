b0de7ede121f13d1d6f4ea26949139a100c7f5e6
Jess Casas <jlcasasg@unal.edu.co>
2018-06-17 21:02:58 -0500

    merge do it!

--- app/models/solicitud.rb
+++ app/models/solicitud.rb
@@ -15,9 +15,8 @@ class Solicitud < ApplicationRecord
     solicitud = all
     solicitud = solicitud.where(estado: estado) if estado && estado != ""
     solicitud = solicitud.where(importancia: importancia) if importancia && importancia != ""
-    solicitud = solicitud.where("nombre_responsable LIKE '%" + responsable + "%' OR nombre_responsable2 LIKE '%" + responsable + "%'") if responsable && responsable != ""
     solicitud = solicitud.where(user: solicitante) if solicitante && solicitante != ""
-    solicitud = solicitud.where(lugar: lugar) if lugar && lugar != ""
+    solicitud = solicitud.joins(:ubicacion).where("ubicacions.lugar = ?", lugar) if lugar && lugar != ""
     return solicitud
   end
 end
